6db2197ec661688c9581a670396974094d9e125b
Mr Anonymous <azhansyakirin@gmail.com>
2023-06-15 00:51:55 +0800

    create style for tab

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./NavBar.scss";
 
 export const NavBar = () => {
+
+    const { pathname } = useLocation();
+    const toLowCase = pathname.toLocaleLowerCase();
+    console.log("Path :::::", toLowCase);
+
     return (
         <nav className="NavBar">
             <ul>
-                <Link to="/home">Home</Link>
-                <Link to="/login.do">Logout</Link>
+                <Link to="/home" className={`${toLowCase === "/home" ? "currentTab" : ""}`}>Home</Link> ||
+                <Link to="/user" className={`${toLowCase === "/user" ? "currentTab" : ""}`}>User</Link> ||
+                <Link to="/login" className={`${toLowCase === "/login" ? "currentTab" : ""}`}>Logout</Link>
             </ul>
         </nav>
     )
