9ca4be03e4e2c38e63d0eadc1df591074088cddf
Mr Anonymous <azhansyakirin@gmail.com>
2023-06-15 00:50:55 +0800

    added new routing page

--- src/services/PageRoute.js
+++ src/services/PageRoute.js
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
+import { User } from "../pages/User";
 
 export const PageRoute = () => {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/login.do' element={<Login />} />
+                <Route path='/login' element={<Login />} />
                 <Route path='/home' element={<Home />} />
+                <Route path='/user' element={<User />} />
             </Routes>
         </BrowserRouter>
     )
